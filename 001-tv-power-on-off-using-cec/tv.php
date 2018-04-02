5c0bacf23e64d5d0e0a167d142cb9858a787ee9f
Mike Hodgson <mike@truckright.ca>
2018-04-02 11:56:21 -0400

    Fix code formatting.

--- 001-tv-power-on-off-using-cec/tv.php
+++ 001-tv-power-on-off-using-cec/tv.php
@@ -1,49 +1,48 @@
 <?php
-	// Key to prevent hackers from potentially controlling your TV!
-	$secret_key = '1234567890';
+  // Key to prevent hackers from potentially controlling your TV!
+  $secret_key = '1234567890';
 
-	// CEC IDs of equipment. Can be single ID or multiple (ie: if also shutting off sound bar)
-	$cec_ids = [0,5];
+  // CEC IDs of equipment. Can be single ID or multiple (ie: if also shutting off sound bar)
+  $cec_ids = [0,5];
 
-	$json = file_get_contents('php://input');
-	$request = json_decode($json, true);
-	process_request($request);
+  $json = file_get_contents('php://input');
+  $request = json_decode($json, true);
+  process_request($request);
 
-	function process_request($request) {
-		global $cec_ids;
+  function process_request($request) {
+    global $cec_ids;
 
-		if (key_authenticated($request['key'])) {
+    if (key_authenticated($request['key'])) {
+      switch(strtolower($request['type'])) {
+        case 'power':
+          toggle_power(strtolower($request['command']));
+          break;
+      }
+    }
+  }
 
-		}
-		switch(strtolower($request['type'])) {
-			case 'power':
-				toggle_power(strtolower($request['command']));
-				break;
-		}
-	}
+  function toggle_power($command) {
+    global $cec_ids;
 
-	function toggle_power($command) {
-		global $cec_ids;
+    foreach ($cec_ids as $cec_id) {
+      switch($command) {
+        case 'on':
+          echo shell_exec("echo 'on $cec_id' | sudo /usr/bin/cec-client -s");
+          break;
+        case 'off':
+          echo shell_exec("echo 'standby $cec_id' | sudo /usr/bin/cec-client -s");
+          break;
+      }
+    }
+  }
 
-		foreach ($cec_ids as $cec_id) {
-			switch($command) {
-				case 'on':
-					echo shell_exec("echo 'on $cec_id' | sudo /usr/bin/cec-client -s");
-					break;
-				case 'off':
-					echo shell_exec("echo 'standby $cec_id' | sudo /usr/bin/cec-client -s");
-					break;
-			}
-		}
-	}
+  function key_authenticated($key) {
+    global $secret_key;
 
-	function key_authenticated($key) {
-		global $secret_key;
-
-		if ($key == $secret_key) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+    if ($key == $secret_key) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 ?>
